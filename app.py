d8480589c4dd488d656f3e37651611991aa4d241
Neeraj08823 <nk08823@gmail.com>
2025-08-15 09:53:34 +0530

    ✨ Completed Local Food Wastage Management System task using My-Sql & Streamlit

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db_config = st.secrets["mysql"]
 def get_connection():
     return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
+        port=st.secrets["mysql"]["port"],
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"]
